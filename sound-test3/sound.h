14ad2b32058f9d855cc75f4f0b2998752093d335
SnailBarbarianMacho <nwk_snail@hotmail.com>
2026-05-26 21:02:10 +0900

    ヘッダドキュメントの修正

--- sound-test3/sound.h
+++ sound-test3/sound.h
@@ -217,21 +217,23 @@
 /** サウンド システムの初期化 */
 void sd6Init(void);
 
-/**
- * SD6 - PFM方式6重和音サウンドを鳴らします
+/** SD6 (PFM方式6重和音サウンド) で音楽を鳴らします
+ * - sdcccall1 対応
  * @see SD6PLAY() ラッパーを使用してください
  */
 u8 sd6play(const u32 param);
 
 /** パラメータの受け渡しを高速化するための, sd6Play() ラッパー
- * - 鳴ってる間は 他の作業はできません
+ * - sdcccall1 対応
+ * - CPU を 100% 消費します. 鳴ってる間は 他の作業はできません
  * - 音が鳴らない場合は, 減衰テーブルやサウンド テータが上位 RAM に存在してないか確認してください
  * @param data      u16   sd6-midi-conv.php で変換したデータのアドレス
- * @param tempo     u8    テンポ. [0, 127]. 0 = 100%, 127=150%
+ * @param tempo     u8    テンポ. [0, 127]
+ * - 256/(256-tempo)*100%で計算します. 0/32/64/96/127 = 100/114/133/160/200%
  * @param can_abort bool  中断可能フラグ. true ならば, F1～F4キーで演奏を中断できます
  * - チャタリング防止のため, 演奏終了前は, F1～F4 キーを離すまで待ちます
  * @return u8 演奏中断となったキーを返します.
- * - KEY9_F1_MASK, KEY9_F2_MASK, KEY9_F3_MASK, KEY9_F4_MASK の bitwise or.
+ * - KEY9_F1_MASK ～ KEY9_F4_MASK の bitwise or.
  * - キーを押さないで終了した場合は, 0 を返します
  * @see sd6play()
  */
